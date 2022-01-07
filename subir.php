3235b44294dac956ecc307a08162a1bd798e2ea4
MRubioMunoz <miguelrubiomunoz@gmail.com>
2022-01-07 18:03:29 +0000

    Creado el repositorio. Subida de imagenes

---
+++ subir.php
@@ -0,0 +1,69 @@
+<?php
+
+var_dump($_FILES)['archivo'];
+
+$llega = isUploadedFile('archivo');
+
+echo '<br>Valor de llegada' . $llega . '<br>';
+var_dump ($llega);
+
+main('archivo');
+
+function main($name){
+    $result = uploadFile($name);
+    if($result){
+        echo 'todo bien';
+    } else {
+        header('location' . $server['HTTP_REFERER']);
+        echo "todo mal";
+    }
+}
+
+function isUploadedFile($name) {
+    return isset($_FILES[$name]);
+}
+
+function isValidUploadedFile($file) {
+    $result = true;
+    $error = $file['error'];
+    $name = $file['name'];
+    $size = $file['size'];
+    $tmp_name = $file['tmp_name'];
+    $type = $file['type'];
+    if($error != 0 || $name == '' || $size == 0 || strpos($type, 'image/') === false || !is_uploaded_file($tmp_name)) {
+        $result = false;
+    } else {
+        $mcType = mime_content_type($tmp_name);
+        if(strpos($mcType, 'image/') === false) {
+            $result = false;
+        }
+    }
+    return $result;
+}
+
+function uploadFile($paramName) {
+    $result = false;
+    if(!isUploadedFile($paramName)) {
+        return false;
+    }
+    $file = $_FILES[$paramName];
+    if(!isValidUploadedFile($file)) {
+        return false;
+    }
+    return moveFile($file);
+}
+
+function moveFile($file) {
+    $target = 'upload';
+    $uniqueName = uniqid('image_');
+    $name = $file['name'];
+    $extension = pathinfo($name, PATHINFO_EXTENSION);
+    $tmp_name = $file['tmp_name'];
+    $uploadedFile = $target . '/' . $uniqueName . '.' . $extension;
+    if(move_uploaded_file($tmp_name, $uploadedFile)) {
+        return [$uploadedFile, $uniqueName . '.' . $extension, $uniqueName, $extension,];
+    }
+    echo 'ha fallado';
+    return false;
+}
+?>
